339f148b41a94d1fb4e7d3c5699e54a2bd26d807
DurantVivado <durantthorvals@gmail.com>
2022-05-17 01:40:12 -0700

    hack on PivotRepair

---
+++ src/connection_solver.cc
@@ -0,0 +1,29 @@
+#include "connection_solver.hh"
+
+#include <thread>
+
+ConnectionSolver::ConnectionSolver(const std::string &host, int16_t port)
+{
+  //connect to the server
+  std::cout<<host<<","<<port<<std::endl;
+  while (!conn.connect(sockpp::inet_address(host, port)))
+  {
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+  }
+}
+
+ConnectionSolver::~ConnectionSolver()
+{
+  conn.close();
+}
+
+//DataSolverInterface
+void ConnectionSolver::read(void *buf, ssize_t size)
+{
+  conn.read_n(buf, size);
+}
+
+void ConnectionSolver::write(void *buf, ssize_t size)
+{
+  conn.write_n(buf, size);
+}
